5a6259db25bf2a2fb70798bc17f5904e6a7803b8
Josh <josh.felmeden@capgemini.com>
2024-04-25 13:46:59 +0100

    Chagned fzf path to be generic home

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/jfelmeden/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/home/jfelmeden/.fzf/bin"
+if [[ ! "$PATH" == */${HOME}/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/${HOME}/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/jfelmeden/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/${HOME}/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/jfelmeden/.fzf/shell/key-bindings.zsh"
+source "/${HOME}/.fzf/shell/key-bindings.zsh"
