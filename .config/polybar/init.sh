2fbb2fda55e9a4071ab948efb0f671ed2b1c2a89
Joshua Felmeden <josh.felmeden@outlook.com>
2024-06-10 20:26:25 +0100

    Minor changes to polybar

--- .config/polybar/init.sh
+++ .config/polybar/init.sh
@@ -9,15 +9,15 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 screens=$(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f6)
 
 if [[ $(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f4 | cut -d"+" -f2- | uniq | wc -l) == 1 ]]; then
-  MONITOR=$(polybar --list-monitors | cut -d":" -f1) TRAY_POS=right polybar main &
+  MONITOR=$(polybar --list-monitors | cut -d":" -f1) polybar main &
 else
   primary=$(xrandr --query | grep primary | cut -d" " -f1)
 
   for m in $screens; do
     if [[ $primary == $m ]]; then
-        MONITOR=$m TRAY_POS=right polybar panel &
+        MONITOR=$m polybar panel &
     else
-        MONITOR=$m TRAY_POS=right polybar panel &
+        MONITOR=$m polybar panel &
     fi
   done
 fi
