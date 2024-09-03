c7c957524d12794cbfd21ec7460eaa10d59a5079
Joshua Felmeden <josh.felmeden@outlook.com>
2024-09-03 17:45:18 +0100

    rofi: updated wifi and powermenu

--- .config/rofi/wifi/wifi.sh
+++ .config/rofi/wifi/wifi.sh
@@ -9,7 +9,8 @@ theme='config'
 # nmcli device wifi connect $bssid
 # notify-send "📶 WiFi Connected"
 
-bssid=$(nmcli device wifi list | sed -n '1!P'| cut -b 9- | cut -d' ' -f2- | rofi -dmenu -theme ${dir}/${theme}.rasi -p " " -lines 10 | awk '{print $1}')
+bssid=$(nmcli device wifi list | sed -n '1!P'| cut -b 9- | cut -d' ' -f2- | rofi -dmenu -theme ${dir}/${theme}.rasi -p " " -lines 10 | awk '{for(i=1;i<=NF;i++) if($i=="Infra"){for(j=1;j<i;j++) printf $j" "; print ""}}' |  sed 's/ $//')
 [ -z "$bssid" ] && exit 1
-nmcli device wifi connect $bssid
-notify-send "󱘖 WiFi Connected"
+echo $bssid
+nmcli device wifi connect "${bssid}"
+notify-send -a "Network" "󱘖  WiFi Connected"
